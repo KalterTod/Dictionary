c89c8bd2639603c424b99d0b8fd4430c8268aaab
Stephen Clark <stephenclark@GNVsclark9DV7L.local>
2014-08-11 13:13:37 -0400

    Initial Commit

---
+++ Dictionary2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dictionary2
+//
+//  Created by Stephen Clark on 8/11/14.
+//  Copyright (c) 2014 Stephen Clark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Dictionary2Tests/Dictionary2Tests.swift
@@ -0,0 +1,35 @@
+//
+//  Dictionary2Tests.swift
+//  Dictionary2Tests
+//
+//  Created by Stephen Clark on 8/11/14.
+//  Copyright (c) 2014 Stephen Clark. All rights reserved.
+//
+
+import XCTest
+
+class Dictionary2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
